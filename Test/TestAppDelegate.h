713a1327f4dc1f76cf3ded697cbd8184e1ba132f
Malcom Gilbert <malcomgilbert@gmail.com>
2011-03-27 00:54:25 -0400

    Initial Commit

---
+++ Test/TestAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TestAppDelegate.h
+//  Test
+//
+//  Created by Malcom Gilbert on 3/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
